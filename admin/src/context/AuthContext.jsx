23a9511ff72b9bda27040617553db28216c8d24c
Sharan Chinthoju <158158398+chinthojusharan@users.noreply.github.com>
2025-09-19 16:57:07 +0530

    Update AuthContext.jsx

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react'
 
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://trendly-backend-yvck.onrender.com"
+    let serverUrl = "https://trendly-backend-k8qd.onrender.com"
 
     let value = {
       serverUrl
