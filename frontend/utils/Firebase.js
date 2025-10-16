9aaa695ba0c03edd013c22ec78bad7d07ca8cab3
chinthojusharan <chinthojusharan@gmail.com>
2025-10-16 10:45:12 +0530

    Save all local changes before pull

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -1,17 +1,18 @@
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
+
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "logintrendly.firebaseapp.com",
+  projectId: "logintrendly",
+  storageBucket: "logintrendly.firebasestorage.app",
+  messagingSenderId: "828358237456",
+  appId: "1:828358237456:web:3209304acff7a9314e5995"
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
 
-
-export {auth , provider}
-
+export {auth, provider}
